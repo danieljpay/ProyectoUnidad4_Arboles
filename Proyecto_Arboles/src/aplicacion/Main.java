22d7d200e9285892a1da89753c7c7cecbad71b1b
Daniel Perez <plupyt_dany@hotmail.com>
2020-05-07 17:38:25 -0500

    Ya lee el csv y crea a los egresados

--- Proyecto_Arboles/src/aplicacion/Main.java
+++ Proyecto_Arboles/src/aplicacion/Main.java
@@ -1,18 +1,28 @@
 package aplicacion;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import modelo.CSV;
+import modelo.Egresado;
 
 /**
  *
  * @author plupy
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Egresado> totalEgresados = new ArrayList<Egresado>();
         System.out.println("comencemos leyendo el csv: ");
         String ruta = "Egresados.csv";
         CSV datos = new CSV();
-        datos.readCSV(ruta);
+        datos.readCSV(ruta, totalEgresados);
+        
+        System.out.println("Esto debería imprimir a Alex: ");
+        System.out.println(totalEgresados.get(2).toString());
+        System.out.println("Esto debería imprimir a Daniel: ");
+        System.out.println(totalEgresados.get(0).toString());
+        System.out.println("Esto debería imprimir a Jimmy: ");
+        System.out.println(totalEgresados.get(1).toString());
     }
     
 }
--- Proyecto_Arboles/src/modelo/CSV.java
+++ Proyecto_Arboles/src/modelo/CSV.java
@@ -11,9 +11,12 @@ import java.util.Arrays;
  * @author plupy
  */
 public class CSV {
-    public void readCSV(String ruta) throws IOException{
+    public void readCSV(String ruta, ArrayList<Egresado> destino) throws IOException{
         /*  fuente:  thttp://www.programandoapasitos.com/2017/04/como-leer-fichero-csv-con-java.htmlhtp://www.programandoapasitos.com/2017/04/como-leer-fichero-csv-con-java.html */
         String separator = ",";
+        String nombre;
+        String profesion;
+        float promedio;
         BufferedReader bufferLectura = null;
         try{
             //abrir el .csv en buffer de lectura
@@ -21,12 +24,15 @@ public class CSV {
             
             //leer una linea del archivo
             String linea = bufferLectura.readLine();
+            linea = bufferLectura.readLine();
             
             while (linea != null) {
                 //separar la linea leída con el separador definido previamente
                 String[] campos = linea.split(separator);
-                System.out.println(Arrays.toString(campos));   
-                
+                nombre = campos[0];
+                profesion = campos[1];
+                promedio = Float.parseFloat(campos[2]);
+                destino.add(new Egresado(nombre, profesion, promedio));
                 //volver a leer otra línea del fichero
                 linea = bufferLectura.readLine();
             }
