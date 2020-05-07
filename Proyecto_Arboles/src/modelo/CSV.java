524028b49e4413f61d2fd09ab73d86c0a1e330b8
Daniel Perez <plupyt_dany@hotmail.com>
2020-05-07 17:16:22 -0500

    ya hace lectura de todo el archivo pero faltan ajustes

--- Proyecto_Arboles/src/modelo/CSV.java
+++ Proyecto_Arboles/src/modelo/CSV.java
@@ -11,7 +11,8 @@ import java.util.Arrays;
  * @author plupy
  */
 public class CSV {
-    public void readCSV(String ruta){
+    public void readCSV(String ruta) throws IOException{
+        /*  fuente:  thttp://www.programandoapasitos.com/2017/04/como-leer-fichero-csv-con-java.htmlhtp://www.programandoapasitos.com/2017/04/como-leer-fichero-csv-con-java.html */
         String separator = ",";
         BufferedReader bufferLectura = null;
         try{
@@ -21,12 +22,16 @@ public class CSV {
             //leer una linea del archivo
             String linea = bufferLectura.readLine();
             
-            while(linea != null){
+            while (linea != null) {
                 //separar la linea leída con el separador definido previamente
                 String[] campos = linea.split(separator);
+                System.out.println(Arrays.toString(campos));   
                 
-                System.out.println(Arrays.toString(campos));
+                //volver a leer otra línea del fichero
+                linea = bufferLectura.readLine();
             }
+            
+            bufferLectura.close();
         }
         catch(IOException e){
             e.printStackTrace();
