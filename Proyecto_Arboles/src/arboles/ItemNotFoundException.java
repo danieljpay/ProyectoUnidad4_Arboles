5baf77d87144451bb9a4e3c29febf5c6a0db72b4
Daniel Perez <plupyt_dany@hotmail.com>
2020-05-10 00:57:22 -0500

    clase ArbolABB agregado y NodoBin modificada

---
+++ Proyecto_Arboles/src/arboles/ItemNotFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arboles;
+
+/**
+ *
+ * @author jorge.reyes
+ */
+public class ItemNotFoundException extends RuntimeException{
+    
+    public ItemNotFoundException(String msg){
+      super(msg);
+    }
+    
+}
+
