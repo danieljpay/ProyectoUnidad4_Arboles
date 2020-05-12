67b2863035a55a703da6b86e20ecb33f8ecc8b09
Daniel Perez <plupyt_dany@hotmail.com>
2020-05-12 09:41:26 -0500

    Agregadas las clases Lista, Nodo, Interfaz2 y Raiz del arbolB, solo se identaron para mejor lectura

--- Proyecto_Arboles/src/modelo/CSV.java
+++ Proyecto_Arboles/src/modelo/CSV.java
@@ -1,11 +1,8 @@
 package modelo;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 /**
